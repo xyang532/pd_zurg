6c96c2d0a9ad51826e13db426e8cd8b263c33663
Young Yang <yxl1896@126.com>
2025-08-07 00:19:40 -0700

    update user agent for torrentio

--- plex_debrid/scraper/services/torrentio.py
+++ plex_debrid/scraper/services/torrentio.py
@@ -12,7 +12,7 @@ session = custom_session()
 
 def get(url):
     headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'}
     try:
         ui_print(f"[torrentio] GET url: {url} ...", ui_settings.debug)
         response = session.get(url, headers=headers, timeout=60)
